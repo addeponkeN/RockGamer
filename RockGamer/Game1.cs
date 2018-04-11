a84bc789333524eeba5d5acd5422f0bf268c69e6
wwh05 <meowe94@hotmail.com>
2018-04-11 19:20:00 +0200

    nicer

--- RockGamer/Game1.cs
+++ RockGamer/Game1.cs
@@ -16,13 +16,6 @@ namespace RockGamer
 
         public Game1()
         {
-            Console.WriteLine("nicer gamser starter       alos more ");
-
-            int asdw = 55;
-            int sum = asdw + 11;
-
-            Console.WriteLine("game start");
-
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
